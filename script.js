46778d5a2badd565e8dc596dfa1905b21e601864
ajexks284 <76217648+ajexks284@users.noreply.github.com>
2021-02-22 19:51:22 +0200

    Add book section. Add functions for rendering and removing books

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const addBookBtn = document.getElementById('addBook');
-
 const modalBg = document.getElementById('modalBg');
 const modal = document.getElementById('modal');
-
 const form = document.getElementById('form');
-
 const addTitle = document.getElementById('addTitle');
 const addAuthor = document.getElementById('addAuthor');
 const addPages = document.getElementById('addPages');
 const isRead = document.getElementById('isRead');
-
 const submitBookBtn = document.getElementById('submitBook');
 
 let myLibrary = [];
@@ -27,14 +23,76 @@ function addBookToLibrary() {
     myLibrary.push(book);
 }
 
+const bookSection = document.getElementById('bookSection');
+function createBooks() {
+    myLibrary.forEach((book, index) => {
+        let bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        bookSection.appendChild(bookDiv);
+
+
+        let titleDiv = document.createElement('div');
+        titleDiv.appendChild(document.createTextNode(`Title: "${book.title}"`));
+        bookDiv.appendChild(titleDiv);
+
+        let authorDiv = document.createElement('div');
+        authorDiv.appendChild(document.createTextNode(`Author: ${book.author}`));
+        bookDiv.appendChild(authorDiv);
+
+        let pagesDiv = document.createElement('div');
+        pagesDiv.appendChild(document.createTextNode(`Pages: ${book.pages}`));
+        bookDiv.appendChild(pagesDiv);
+
+        let readDiv = document.createElement('div');
+        readDiv.appendChild(document.createTextNode(`Read: ${book.read ? 'yes' : 'no'}`));
+        bookDiv.appendChild(readDiv);
+
+        let removeBtn = document.createElement('button');
+        removeBtn.addEventListener('click', (e) => {
+            removeBook(e);
+        });
+        removeBtn.appendChild(document.createTextNode('Remove'));
+        removeBtn.classList.add('remove-btn');
+        bookDiv.appendChild(removeBtn);
+    })
+}
+
+function removeBook(e) {
+    //Remove book from array
+    let bookIndexToRemove = [...bookSection.children].indexOf(e.target.parentElement);
+    myLibrary.splice(bookIndexToRemove, 1);
+
+    //Remove book from DOM
+    bookSection.removeChild(e.target.parentElement);
+}
+
+function checkForDuplicateBook() {
+    let isDuplicate = false;
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].title === addTitle.value && myLibrary[i].author === addAuthor.value) {
+            isDuplicate = true;
+            break;
+        }
+    }
+
+    return isDuplicate;
+}
 
 // Event Listeners
 form.addEventListener('submit', () => {
-    addBookToLibrary();
 
-    // reset form after adding book
+    if (!checkForDuplicateBook()) {
+        addBookToLibrary();
+        bookSection.innerHTML = '';
+        createBooks();
+    } else {
+        alert('This book already exists!');
+    }
+
+    // reset form
     form.reset();
-    modalBg.style.display = 'none'
+    modalBg.style.display = 'none';
 });
 
 // modal events
@@ -45,5 +103,6 @@ addBookBtn.addEventListener('click', () => {
 window.addEventListener('click', (e) => {
     if (e.target == modalBg) {
         modalBg.style.display = 'none';
+        form.reset();
     }
 })
